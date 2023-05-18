1088c9802638190e598cbd310af181749368bcf8
Aryan Shukla <88445101+Telomelonia@users.noreply.github.com>
2023-05-18 16:22:10 +0530

    Update event-utils.js

--- src/event-utils.js
+++ src/event-utils.js
@@ -1,20 +1,23 @@
-
-let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+let eventGuid = 0;
+let todayStr = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS = [
   {
     id: createEventId(),
-    title: 'All-day event',
-    start: todayStr
+    title: 'Birthday',
+    start: '2023-05-08',
   },
   {
     id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  }
-]
-
+    title: 'Orientation',
+    start: todayStr,
+  },
+  {
+    id: createEventId(),
+    title: 'Meeting',
+    start: todayStr + 'T12:30:00',
+  },
+];
 export function createEventId() {
-  return String(eventGuid++)
+  return String(eventGuid++);
 }
