07a9e0352e2972da11257f553f17c56917fa3337
Aryan Shukla <88445101+Telomelonia@users.noreply.github.com>
2023-05-18 16:22:35 +0530

    Update DemoApp.jsx

--- src/DemoApp.jsx
+++ src/DemoApp.jsx
@@ -1,56 +1,60 @@
-import React from 'react'
-import { formatDate } from '@fullcalendar/core'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
+import React from 'react';
+import { formatDate } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
 
 export default class DemoApp extends React.Component {
-
   state = {
     weekendsVisible: true,
-    currentEvents: []
-  }
+    currentEvents: [],
+  };
 
   render() {
     return (
-      <div className='demo-app'>
+      <div className="demo-app">
         {this.renderSidebar()}
-        <div className='demo-app-main'>
+        <div className="demo-app-main">
           <FullCalendar
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             headerToolbar={{
               left: 'prev,next today',
               center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+              right: 'dayGridMonth,timeGridWeek,timeGridDay',
             }}
-            initialView='dayGridMonth'
+            initialView="dayGridMonth"
             editable={true}
             selectable={true}
             selectMirror={true}
             dayMaxEvents={true}
             weekends={this.state.weekendsVisible}
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            initialEvents={INITIAL_EVENTS}
             select={this.handleDateSelect}
-            eventContent={renderEventContent} // custom render function
+            eventContent={renderEventContent}
             eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
-            eventAdd={function(){}}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
+            eventsSet={this.handleEvents}
           />
         </div>
       </div>
-    )
+    );
   }
-
+  sidebar2() {
+    return (
+      <div id="main">
+        <button class="openbtn" onclick="openNav()">
+          &#9776; Open Sidebar
+        </button>
+        <h2>Collapsed Sidebar</h2>
+        <p>Content...</p>
+      </div>
+    );
+  }
   renderSidebar() {
     return (
-      <div className='demo-app-sidebar'>
-        <div className='demo-app-sidebar-section'>
+      <div className="demo-app-sidebar">
+        <div className="demo-app-sidebar-section">
           <h2>Instructions</h2>
           <ul>
             <li>Select dates and you will be prompted to create a new event</li>
@@ -58,37 +62,35 @@ export default class DemoApp extends React.Component {
             <li>Click an event to delete it</li>
           </ul>
         </div>
-        <div className='demo-app-sidebar-section'>
+        <div className="demo-app-sidebar-section">
           <label>
             <input
-              type='checkbox'
+              type="checkbox"
               checked={this.state.weekendsVisible}
               onChange={this.handleWeekendsToggle}
             ></input>
             toggle weekends
           </label>
         </div>
-        <div className='demo-app-sidebar-section'>
+        <div className="demo-app-sidebar-section">
           <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>
-            {this.state.currentEvents.map(renderSidebarEvent)}
-          </ul>
+          <ul>{this.state.currentEvents.map(renderSidebarEvent)}</ul>
         </div>
       </div>
-    )
+    );
   }
 
   handleWeekendsToggle = () => {
     this.setState({
-      weekendsVisible: !this.state.weekendsVisible
-    })
-  }
+      weekendsVisible: !this.state.weekendsVisible,
+    });
+  };
 
   handleDateSelect = (selectInfo) => {
-    let title = prompt('Please enter a new title for your event')
-    let calendarApi = selectInfo.view.calendar
+    let title = prompt('Please enter a new title for your event');
+    let calendarApi = selectInfo.view.calendar;
 
-    calendarApi.unselect() // clear date selection
+    calendarApi.unselect(); // clear date selection
 
     if (title) {
       calendarApi.addEvent({
@@ -96,23 +98,26 @@ export default class DemoApp extends React.Component {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      })
+        allDay: selectInfo.allDay,
+      });
     }
-  }
+  };
 
   handleEventClick = (clickInfo) => {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove()
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
     }
-  }
+  };
 
   handleEvents = (events) => {
     this.setState({
-      currentEvents: events
-    })
-  }
-
+      currentEvents: events,
+    });
+  };
 }
 
 function renderEventContent(eventInfo) {
@@ -121,14 +126,29 @@ function renderEventContent(eventInfo) {
       <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
     </>
-  )
+  );
 }
 
 function renderSidebarEvent(event) {
   return (
     <li key={event.id}>
-      <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
+      <b>
+        {formatDate(event.start, {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        })}
+      </b>
       <i>{event.title}</i>
     </li>
-  )
+  );
+}
+function openNav() {
+  document.getElementById('mySidebar').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeNav() {
+  document.getElementById('mySidebar').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
 }
